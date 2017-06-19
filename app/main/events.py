689d2e09f78c3e284ffd1d697bcdc0cc8df3f55c
alljp <allenjosesmailbox@gmail.com>
2017-06-19 12:10:57 +0530

    Add storeMessage() method

--- app/main/events.py
+++ app/main/events.py
@@ -1,26 +1,31 @@
 from flask import session
 from flask_socketio import emit, join_room, leave_room
 from .. import socketio
+from .models import storeMessage
 
 
 @socketio.on('joined', namespace='/chat')
 def joined(message):
     room = session.get('room')
     join_room(room)
-    emit('status', {'msg': session.get('name') +
-                    ' has entered the room.'}, room=room)
+    msg = session.get('name') + ' has entered the room.'
+    print("\n\n", msg)
+    emit('status', {'msg': msg}, room=room)
+    storeMessage(msg, room)
 
 
 @socketio.on('text', namespace='/chat')
 def text(message):
     room = session.get('room')
-    emit('message', {'msg': session.get('name') +
-                     ':' + message['msg']}, room=room)
+    msg = session.get('name') + ':' + message['msg']
+    emit('message', {'msg': msg}, room=room)
+    storeMessage(msg, room)
 
 
 @socketio.on('left', namespace='/chat')
 def left(message):
     room = session.get('room')
     leave_room(room)
-    emit('status', {'msg': session.get('name') +
-                    ' has left the room.'}, room=room)
+    msg = session.get('name') + ' has left the room.'
+    emit('status', {'msg': msg}, room=room)
+    storeMessage(msg, room)
